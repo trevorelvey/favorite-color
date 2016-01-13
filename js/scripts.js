04bc56a79678e5a64ab4a6ca7e4cb6ce9848f627
gracie lundell and trevor elvey <trevor.elvey@gmail.com>
2016-01-12 16:18:49 -0800

    Add more color options

--- js/scripts.js
+++ js/scripts.js
@@ -13,4 +13,19 @@ $(document).ready(function() {
     $("body").removeClass();
     $("body").addClass("red-background");
   });
+
+  $("button#purple").click(function() {
+    $("body").removeClass();
+    $("body").addClass("purple-background");
+  });
+
+  $("button#blue").click(function() {
+    $("body").removeClass();
+    $("body").addClass("blue-background");
+  });
+
+  $("button#reset").click(function() {
+    $("body").removeClass();
+    $("body").addClass("reset-background");
+  });
 });
